be35a4467efc7ff104b9e6850cc3c0d3ee48060e
Allison <alvesallison42@gmail.com>
2025-01-14 21:12:01 -0300

    Contador Adicionado

--- src/pages/AdminPainel/AdminPanel.jsx
+++ src/pages/AdminPainel/AdminPanel.jsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from 'react';
+import './AdminPanel.css';
+import Menu from '../../components/Menu/menu';
 
 const fetchServicesFromLocalStorage = () => {
   const savedServices = localStorage.getItem('services');
@@ -23,19 +25,32 @@ const AdminPanel = () => {
     localStorage.setItem('services', JSON.stringify(updatedServices));
   };
 
+  function contar() {
+    const qtd = fetchServicesFromLocalStorage()
+    let cont = qtd.length
+    return cont
+  }
+
   return (
-    <div>
+    <div className='container-painel-admin'>
+
+      <Menu/>
+
       <h1>Painel Administrativo</h1>
       <h2>Serviços Disponíveis</h2>
-
+      <h3 id='mostrar'> {contar()} </h3>
       <ul>
         {services.map(service => (
+
+          
           <li key={service.id}>
             Serviço: {service.name} - Descrição {service.description} - Hora: {service.price} - Data: {service.data}
             <button onClick={() => deleteService(service.id)}>Excluir</button>
           </li>
         ))}
       </ul>
+
+
     </div>
   );
 };
